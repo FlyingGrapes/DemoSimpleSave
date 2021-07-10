a65c1b613eb9689f2a25e86a9a8c23264522b8d7
GRACE NG <83058766+FlyingGrapes@users.noreply.github.com>
2021-07-10 14:41:11 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20018621/demosimplesave/MainActivity.java
@@ -0,0 +1,43 @@
+package sg.edu.rp.c346.id20018621.demosimplesave;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+//        String msg = "No greetings";
+//        Toast toast = Toast.makeText(MainActivity.this, msg, Toast.LENGTH_LONG);
+//        toast.show();
+        // Step 1: Obtain the SharedPreferences instance
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+        // Step 2: Retrieve the saved data from the SharedPreference
+        // with a default value if no matching data
+        String msg = prefs.getString("greetings", "No greetings name");
+        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_LONG);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Step 1: Obtain the SharedPreferences instance
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+        // Step 2: Create a SharedPreferences Editor by calling edit()
+        SharedPreferences.Editor prefEdit = prefs.edit();
+        // Step 3: Set a key-value pair in the editor
+        prefEdit.putString("greetings", "Hello!");
+        // Step 4: Call commit() to save the changes made to the SharedPreference.
+        prefEdit.commit();
+    }
+}
